c8efb454211ea5b781dd74df25b6b240fea56582
Shahriar <shahriar3809@gmail.com>
2024-05-10 23:29:54 +0600

    return functionality added

--- src/components/BorrowedCard.jsx
+++ src/components/BorrowedCard.jsx
@@ -1,17 +1,34 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
+import axios from "axios";
+import { toast } from "react-toastify";
+
 
 const BorrowedCard = ({book}) => {
     const {
-      _id,
+      // _id,
       bookName,
       photoUrl,
       borrowedDate,
       category_name,
       returnDate,
+      bookId
     } = book;
-    // console.log(book)
+    // console.log(bookId)
+
+
+
+    const handleReturn = (bookId) => {
+    
+      axios.delete(`http://localhost:5000/returnBook/${bookId}`)
+      .then(res=> {
+        console.log(res.data)
+        toast.success('Return Successful.')
+      })
+    }
+
+
     return (
       <div>
         <div className="w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-lg dark:bg-green-700">
@@ -38,7 +55,7 @@ const BorrowedCard = ({book}) => {
 
             <div className="flex items-center justify-between mt-4 text-gray-700 dark:text-gray-200">
               <h1 className="px-2 font-bold text-xl"> Borrowed Date: {borrowedDate}</h1>
-              <button className="btn btn-primary">Return</button>
+              <button onClick={()=> handleReturn(bookId)} className="btn btn-primary">Return</button>
             </div>
           </div>
         </div>
